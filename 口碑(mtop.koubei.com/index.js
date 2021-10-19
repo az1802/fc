0f3544f5d445f41a82780db2a1bd34ffa139c04f
等待待等 <317474545@qq.com>
2021-10-19 10:55:02 +0800

    口碑网脚本调整

--- 口碑(mtop.koubei.com/index.js
+++ 口碑(mtop.koubei.com/index.js
@@ -3,18 +3,18 @@ const fs = require("fs");
 const path = require("path");
 
 
-const { requestUrl,genImgs,genExcel,genExcelAll,genWord,genSpecificationsWord,formatFileName,delDirSync,mkdirSync,genShilaiExcelAll} = require("../utils/index")
+const { requestUrl,genImgs,genExcel,genExcelAll,genWord,genSpecificationsWord,formatFileName,delDirSync,mkdirSync,genShilaiExcelAll,genFeieExcelAll} = require("../utils/index")
 
 
 
 
 // const exportMode = "keruyun"
-// const exportMode = "feie"
+const exportMode = "feie"
 
-const exportMode = "shilai"
+// const exportMode = "shilai"
 
 let allMerchantInfo = require("./merchantInfo.json")
-let requestShopData = {name:"闽台风味小吃",shop_pic:""}
+let requestShopData = {name:"昌记隆江猪脚饭",shop_pic:""}
 let requestMenuData = allMerchantInfo.data.data.components[2].data.dishMenu.data.dishCates
 
 const { isRegExp } = require("util");
@@ -25,21 +25,17 @@ const { isRegExp } = require("util");
 const outputDir = path.join(__dirname, "merchantInfos")
 
 let menuSetting = { //到处的菜品属性归为规格,备注,加料,做法
-  specifications:[],//规格
+  specifications:[ "规格默认",],//规格
   practice: [
     "默认",
-    "规格默认",
-    "口味",
-    "请选择"
+    "备注"
   ],//做法
-  feeding:[],//加料
+  feeding:[ ],//加料
   remarks: [],//备注
   propsGroupSort: [
-    "默认",
     "规格默认",
-    "口味",
-    "请选择"
-   
+    "默认",
+    "备注",
   ],
   propsSort: {
     // "口味":["不辣","微辣","中辣","特辣","麻辣"]
@@ -68,13 +64,27 @@ function addPropsGroupArr(propsGroupArr,name) {
 }
 
 function formatFoodProps(foodItem) {
+  let propsRes = [];
   
-  let cookbookDishSku = foodItem.cookbookDishSkuList && foodItem.cookbookDishSkuList[0];
+  let cookbookDishSku = foodItem.cookbookDishSkuList && foodItem.cookbookDishSkuList[0]; //处理规格
+  if (foodItem.cookbookDishSkuList.length) {
+    let temp = {
+      name:"规格默认",
+      values:foodItem.cookbookDishSkuList.map(comboItem => {
+        return {
+          value: comboItem.skuName,
+          price: parseFloat( comboItem.sellPrice || 0)/100,
+          propName:"规格默认",
+          isMul:false
+        }
+      })
+    }
+    propsRes.push(temp)
+  }
 
-  let propsRes = [], practiceGroupsTemp = [], sideDishGroupsTemp = [];
-  let { practiceGroups = [], sideDishGroups = [] } = cookbookDishSku || {}
+  let practiceGroupsTemp = [], sideDishGroupsTemp = [];
+  let { practiceGroups = [], sideDishGroups = [] ,remarks=[]} = cookbookDishSku || {}
   let {specGroupList = [] } = foodItem
-
 
 
   practiceGroups.forEach((practiceGroupItem,index) => {
@@ -105,19 +115,19 @@ function formatFoodProps(foodItem) {
     })
   })
 
-  specGroupList.forEach(sepcItem => {
+  if (remarks.length) {
     propsRes.push({
-      name: sepcItem.groupName,
-      values: sepcItem.specs && sepcItem.specs.map(item => {
+      name: "备注",
+      values: remarks.map(remarkItem => {
         return {
-          value: item.name,
-          price: parseFloat( item.addPrice || 0)/100,
-          propName:sepcItem.groupName,
+          value: remarkItem.name,
+          price: parseFloat( remarkItem.addPrice || 0)/100,
+          propName:"备注",
           isMul:true
         }
       }) || []
     })
-  })
+  }
 
   return propsRes;
 }
@@ -139,12 +149,13 @@ async function  handleRequestData(requestShopData,requestMenuData) {
 
     // 菜品目录
     let categories = []
+
     categories = requestMenuData.map(categoryItem => { 
       let categoryData = {
         name: "",
         foods:[]
       };
-      categoryData.name = categoryItem.decorateData.name;
+      categoryData.name = categoryItem.name;
       categoryData.foods = categoryItem.dishList.reduce((res, foodItem) => {
         if (foodItem) {
           let cookbookDishSku = foodItem.cookbookDishSkuList && foodItem.cookbookDishSkuList[0];
@@ -195,12 +206,14 @@ async function genImgsAndExcel() {
 
   // // // mkShopDir(merchantInfo)
   if (exportMode == "keruyun") {
-    genImgs(merchantInfo,outputDir);
+    genImgs(merchantInfo, outputDir);
     genExcel(merchantInfo, outputDir);
-    genExcelAll(merchantInfo,outputDir,menuSetting)
+    genExcelAll(merchantInfo, outputDir, menuSetting)
   } else if (exportMode == 'shilai') {
     genShilaiExcelAll(merchantInfo, outputDir, menuSetting)
-  }else {
+  } else if (exportMode == 'feie') {
+    genFeieExcelAll(merchantInfo, outputDir, menuSetting)
+  }else{  
     genWord(merchantInfo, outputDir)
     genSpecificationsWord(merchantInfo,outputDir,menuSetting)
   }
