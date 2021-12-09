742970e47ded7ef5c7b2f957f0f11e61ef86c0ad
fuheideMayuyu <xiaguangding@163.com>
2021-12-09 12:16:35 +0800

    .

--- 口碑(mtop.koubei.com/index.js
+++ 口碑(mtop.koubei.com/index.js
@@ -9,13 +9,13 @@ const { requestUrl,genImgs,genExcel,genExcelAll,genWord,genSpecificationsWord,fo
 
 
 // const exportMode = "keruyun"
-const exportMode = "feie"
+// const exportMode = "feie"
 
-// const exportMode = "shilai"
+const exportMode = "shilai"
 
 let allMerchantInfo = require("./merchantInfo.json")
 let requestShopData = {name:"清麦牛肉面)",shop_pic:""}
-let requestMenuData = allMerchantInfo.data.data.components[2].data.dishMenu.data.dishCates
+let requestMenuData = allMerchantInfo.data.data.components[3].data.dishMenu.data.dishCates
 
 const { isRegExp } = require("util");
 
@@ -27,10 +27,15 @@ const outputDir = path.join(__dirname, "merchantInfos")
 let menuSetting = { //到处的菜品属性归为规格,备注,加料,做法
   specifications:[ "规格默认",],//规格
   practice: [
+    "规格默认",
     "默认",
     "备注",
     '牛肉汤',
-    "打包"
+    "打包",
+    "用餐类型",
+    "米粉类",
+    "辣度",
+    "小吃加料"
   ],//做法
   feeding:[ ],//加料
   remarks: [],//备注
@@ -39,7 +44,11 @@ let menuSetting = { //到处的菜品属性归为规格,备注,加料,做法
     "默认",
     "备注",
     '牛肉汤',
-    "打包"
+    "打包",
+    "用餐类型",
+    "米粉类",
+    "辣度",
+    "小吃加料"
   ],
   propsSort: {
     // "口味":["不辣","微辣","中辣","特辣","麻辣"]
