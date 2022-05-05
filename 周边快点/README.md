a8ce30c0bb8c1bda427234236658cf6870beb576
sunj <317474545@qq.com>
2022-05-05 12:31:15 +0800

    优化脚本

---
+++ 周边快点/README.md
@@ -0,0 +1,3 @@
+GGYPd 直接解析二维码即可获得该参数值
+
+https://m-diancan.lehuipay.com/api/mini/GGYPd
--- 周边快点/index.js
+++ 周边快点/index.js
@@ -2,26 +2,26 @@
 const fs = require("fs");
 const path = require("path");
 const requestMenuJson = require("./merchantInfo.json");
-let merchantMenuInfo = requestMenuJson;
+let merchantMenuInfo = requestMenuJson.data;
 
 let shopeInfo = merchantMenuInfo.merchant
+console.log('shopeInfo: ', shopeInfo);
 let foodList = merchantMenuInfo.goods
 let subGroups = merchantMenuInfo.subGroups;
 let specifications = merchantMenuInfo.specifications;
 
-const { requestUrl,genImgs,genExcel,genExcelAll,genWord,genSpecificationsWord,formatFileName,delDirSync,mkdirSync} = require("../utils/index")
 
-
+const { requestUrl, genImgs, genExcel, genExcelAll, genWord, genSpecificationsWord, formatFileName, delDirSync, mkdirSync, genFeieExcelAll, genShilaiExcelAll } = require("../utils/index")
 // const exportMode = "keruyun"
 const exportMode = "feie"
 
 let menuSetting = { //到处的菜品属性归为规格,备注,加料,做法
   specifications:[],//规格
-  practice:[ '就餐类型', '整只或切块', '规格','做法'],//做法
+  practice:[ '就餐类型', '整只或切块', '规格','做法',"主食","份量"],//做法
   feeding:[],//加料
   remarks: [],//备注
   propsGroupSort: [
-    '就餐类型', '整只或切块', '规格','做法'
+    '就餐类型', '整只或切块', '规格','做法',"主食","份量"
   ],
   propsSort: {
   }
@@ -174,11 +174,15 @@ async function genImgsAndExcel() {
 
   // // mkShopDir(merchantInfo)
   if (exportMode == "keruyun") {
-    genImgs(merchantInfo,outputDir);
+    genImgs(merchantInfo, outputDir);
     genExcel(merchantInfo, outputDir);
-    genExcelAll(merchantInfo,outputDir,menuSetting)
+    genExcelAll(merchantInfo, outputDir, menuSetting)
+  } else if (exportMode == 'shilai') {
+    genShilaiExcelAll(merchantInfo, outputDir, menuSetting, false)
   } else {
-    genSpecificationsWord(merchantInfo, outputDir,menuSetting)
+    // genWord(merchantInfo, outputDir)
+    // genSpecificationsWord(merchantInfo, outputDir, menuSetting)
+    genFeieExcelAll(merchantInfo, outputDir, menuSetting)
   }
 }
 
